6b4e93a8e2e804d88a5f3060fee72e8dbc956ce1
Carlos Scarinci <cscarinci@dgist.ac.kr>
2025-06-08 21:44:32 +0900

    add testing repository

--- scripts/boxkit.sh
+++ scripts/boxkit.sh
@@ -4,5 +4,6 @@
 ./distrobox-shims.sh
 
 # Update the container and install packages
+echo "http://dl-cdn.alpinelinux.org/alpine/edge/testing" | tee -a /etc/apk/repositories
 apk update && apk upgrade
 grep -v '^#' ./boxkit.packages | xargs apk add
